e9e5b35d4c7955a9068deb77e86f56a91d1e4767
Shunsuke Umezawa <s20c1022hb@s.chibakoudai.jp>
2022-01-12 11:06:50 +0000

    Initial commit

--- scripts/twice.py
+++ scripts/twice.py
@@ -6,13 +6,49 @@ n = 0
 
 def cb(message):
     global n
-    n = message.data*2
+    n = message.data
 
 if __name__ == '__main__':
     rospy.init_node('twice')
     sub = rospy.Subscriber('count_up', Int32,cb)
     pub = rospy.Publisher('twice', Int32, queue_size=1)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
+        if n % 2 == 0 and n != 0:
+
+            if n % 3 == 0:
+                print('[%d]は\n2の倍数' % n)
+
+                if n % 5 == 0:
+                    print('3の倍数\n5の倍数です。\n')
+
+                else:
+                    print('3の倍数です。\n')
+
+            elif n % 5 == 0:
+                print('[%d]は\n2の倍数\n5の倍数です。\n' % n)
+
+            else:
+                print('[%d]は\n2の倍数です。\n' % n)
+
+        if n % 2 != 0 and n % 3 == 0:
+
+            if n % 5 == 0: 
+                print('[%d]は\n3の倍数\n5の倍数です。\n' % n)
+
+            else:
+                print('[%d]は\n3の倍数です。\n' % n)
+
+        if n % 2 != 0 and n % 3 != 0:
+
+            if n % 5 == 0:
+                print('[%d]は\n5の倍数です。\n' % n)
+
+            else:
+                print('[%d]は\n2.3.5の倍数ではありません。\n' % n)
+
+        if n == 0:
+            print('[%d]は\n2.3.5の倍数ではありません。\n' % n)
+
         pub.publish(n)
         rate.sleep()
